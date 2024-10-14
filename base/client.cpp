1abbdeed8eb2c02f0a334248eb98abd4c6193f34
EgorWeders <dev.weders@gmail.com>
2024-10-14 11:56:47 +0700

    Adds methods which can work with preallocated id containers

--- base/client.cpp
+++ base/client.cpp
@@ -3,85 +3,85 @@
 Client::Client() {}
 
 bool Client::sendMessage(Client *reciever) {
-	if (!reciever)
-		return false;
-	if (senderQueue.size() <= 0)
-		return false;
-	Message tempMsgd = senderQueue.back();
-	if (this->sendMutator)
-		tempMsgd.applyFilter(sendMutator);
-	senderQueue.pop_back();
-	return reciever->receiveMessage(tempMsgd);
+  if (!reciever)
+    return false;
+  if (senderQueue.size() <= 0)
+    return false;
+  Message tempMsgd = senderQueue.back();
+  if (this->sendMutator)
+    tempMsgd.applyFilter(sendMutator);
+  senderQueue.pop_back();
+  return reciever->receiveMessage(tempMsgd);
 }
 
-size_t Client::sendAll(Client *reciever)
-{
-    int res = 0;
-	;
-	while (!senderQueue.empty()) {
-		res += sendMessage(reciever);
-	}
-	return res;
+size_t Client::sendAll(Client *reciever) {
+  int res = 0;
+  ;
+  while (!senderQueue.empty()) {
+    res += sendMessage(reciever);
+  }
+  return res;
 }
 
 bool Client::receiveMessage(const Message &msg) {
-	if (!msg.isValid())
-		return false;
-	if (this->recvMutator) {
-		Message tempMsgd = msg;
-		tempMsgd.applyFilter(recvMutator);
+  if (!msg.isValid())
+    return false;
+  if (this->recvMutator) {
+    Message tempMsgd = msg;
+    tempMsgd.applyFilter(recvMutator);
 
-		recvQueue.insert(recvQueue.begin(), tempMsgd);
-	} else {
-		recvQueue.insert(recvQueue.begin(), msg);
-	}
-	return true;
+    recvQueue.insert(recvQueue.begin(), tempMsgd);
+  } else {
+    recvQueue.insert(recvQueue.begin(), msg);
+  }
+  return true;
 }
 
 size_t Client::appendMessage(const char body[], size_t size) {
-	char *id = new char[6];
-    strlcpy(id, "dummy", 6);
-    if (idGen) {
-        delete[] id;
-		id = idGen->newId();
+  char *id;
+  if (idGen) {
+    id = idGen->newId();
+  } else {
+    id = new char[6];
+    memcpy(id, "dummy", 6);
+  }
+  if (size > Message::maxMessageSize()) {
+    size_t counter = 0;
+    size_t begin = 0;
+
+    while (size > 0) {
+      size_t msgSize = std::min(Message::maxMessageSize(), size);
+      Message tempMsgd(id, body + begin, msgSize);
+      if (tempMsgd.isValid()) {
+        senderQueue.insert(senderQueue.begin(), tempMsgd);
+        counter++;
+      }
+
+      begin += msgSize;
+      size -= msgSize;
     }
-    if (size > Message::maxMessageSize()) {
-		size_t counter = 0;
-		size_t begin   = 0;
-
-		while (size > 0) {
-			size_t  msgSize = std::min(Message::maxMessageSize(), size);
-			Message tempMsgd(id, body + begin, msgSize);
-			if (tempMsgd.isValid()) {
-				senderQueue.insert(senderQueue.begin(), tempMsgd);
-				counter++;
-			}
-
-			begin += msgSize;
-			size -= msgSize;
-		}
-		delete[] id;
-		return counter;
-	}
-	Message tempMsgd(id, body, size);
-	delete[] id;
-	if (!tempMsgd.isValid()) {
-		return false;
-	}
-	senderQueue.insert(senderQueue.begin(), tempMsgd);
-	return true;
+    delete[] id;
+    return counter;
+  }
+  Message tempMsgd(id, body, size);
+  delete[] id;
+  if (!tempMsgd.isValid()) {
+    return false;
+  }
+  senderQueue.insert(senderQueue.begin(), tempMsgd);
+  return true;
 }
 
 Message Client::getLastMessage() const {
-	if (recvQueue.empty())
-		return Message::NullMessage();
-	return recvQueue.back();
+  if (recvQueue.empty())
+    return Message::NullMessage();
+  return recvQueue.back();
 }
 
 Message Client::takeLastMessage() {
-	if (recvQueue.empty())
-		return Message::NullMessage();
-	auto res = recvQueue.back();
-	recvQueue.pop_back();
-	return res;
+  if (recvQueue.empty())
+    return Message::NullMessage();
+  auto res = recvQueue.back();
+  recvQueue.pop_back();
+  return res;
 }
--- base/idgenerator.h
+++ base/idgenerator.h
@@ -2,26 +2,34 @@
 #define IDGENERATOR_H
 #include <atomic>
 #include <cstring>
+#include <string.h>
 #include <string>
 class IdGenerator {
 
 public:
-	IdGenerator(){};
-	virtual char *newId() = 0;
-	virtual void  reset() = 0;
+  IdGenerator(){};
+  virtual char *newId() = 0;
+  virtual char *newId(char *idDst) = 0;
+  virtual size_t idSize() = 0;
+  virtual void reset() = 0;
 };
 class IdCounterGenerator : public IdGenerator {
-	std::atomic_int counter = 0;
+  std::atomic_int counter = 0;
 
 public:
-    IdCounterGenerator(){};
-    char *newId() override {
-		std::string strData = std::to_string(counter++);
-		char *      temp    = new char[strData.length() + 1];
-        strlcpy(temp, strData.c_str(), strData.size() + 1);
-        return temp;
-    }
-	void reset() override { counter = 0; };
+  IdCounterGenerator(){};
+  char *newId() override {
+    char *temp = new char[idSize() + 1];
+
+    return newId(temp);
+  }
+  char *newId(char *idDst) override {
+    std::string strData = std::to_string(counter++);
+    memcpy(idDst, strData.data(), strData.size() + 1); // null term
+    return idDst;
+  }
+  size_t idSize() override { return std::to_string(counter++).size(); }
+  void reset() override { counter = 0; };
 };
 
 #endif // IDGENERATOR_H
